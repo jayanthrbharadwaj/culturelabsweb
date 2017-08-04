eb2743a173c179488618a37fd10a6eb39b8bc367
jayanth.bharadwaj <jayanth.bharadwaj@verse.in>
2017-08-05 00:48:41 +0530

    fixed again with href

--- app/javascripts/share/ShareUrlBuilder.js
+++ app/javascripts/share/ShareUrlBuilder.js
@@ -3,7 +3,7 @@
  */
 class ShareUrlBuilder {
     static  createShareUrl(itemObj) {
-        var baseUrl = window.location.origin
+        var baseUrl = window.location.href
         baseUrl = baseUrl + "?&id=" + itemObj.sys.id
         return baseUrl;
     }
