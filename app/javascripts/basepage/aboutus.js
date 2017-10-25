92bbcd5cdfe4ae0b3e64e2f11dfe3203edc999a5
Vivek Hegade M S <vivekhegade0407@gmail.com>
2017-10-25 21:41:17 +0530

    Changed the code according to the review

--- app/javascripts/basepage/aboutus.js
+++ app/javascripts/basepage/aboutus.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {List, ListItem} from 'material-ui/List';
 import {Tabs, Tab} from 'material-ui/Tabs';
-import SwipeableViews from 'react-swipeable-views';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import Checkbox from 'material-ui/Checkbox';
@@ -16,31 +14,31 @@ class App extends React.Component {
             fontSize: '20px',
             padding: '10px',
         }
-		const styletext = {
-			color: 'red',
-		}
+	const styletext = {
+		color: 'red',
+	}
 
         return (
             <MuiThemeProvider>
                 <div>
                     <h1>Madhwa Sangraha</h1>
 
-                    <Tabs>
-						<Tab label="About Us">
-							<div>
+                <Tabs>
+			<Tab label="About Us">
+			<div>
                                 <h1 style={styletext}>About Us</h1>
                                 <hr style={hrstyle}/>
                                 <p>We are Team based out of Bengaluru. Our core competency is in creating a platform for all our users who actively contribute
 								   to the culture they are brought up in. Pass on the joy of being a part of this platform and involve your own community and collaborate</p>
-                                <hr style={hrstyle}/>
+                                <hr/>
 								<h4>Contact-us</h4>
                                 <TextField hintText="yourname" floatingLabelText="Name"/><br/>
                                 <TextField hintText="youremail" floatingLabelText="Email Id"/><br/>
                                 <TextField hintText="yourfeedback" floatingLabelText="Feedback" multiLine={true}/><br/>
                                 <Checkbox label="Please tick checkbox below. Incase we reach out to you directly"/><br/>
                                 <RaisedButton label="Submit" style="margin:12;"/>
-                            </div>
-						</Tab>
+                        </div>
+			</Tab>
                     </Tabs>
                 </div>
             </MuiThemeProvider>
